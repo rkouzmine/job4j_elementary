653268ddc1a514089a7500a8b91a9b5758e2306d
ruzz3ll <krrsea@gmail.com>
2023-10-02 12:20:01 +0300

    1. Определите тип данных для переменных. [#172634 #449284]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ruslan Kuzmin");
+        System.out.println("21.02.1992");
+    }
+}
