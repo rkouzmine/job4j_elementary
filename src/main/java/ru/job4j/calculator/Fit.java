9c7d04a27fc764ed66b68188e98344f5c712c43b
ruzz3ll <krrsea@gmail.com>
2023-10-03 09:51:43 +0300

    6. Идеальный вес [#41584]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,12 +2,12 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        double result = height - 100 * 1.15;
+        double result = (height - 100) * 1.15;
         return result;
     }
 
     public static double womanWeight(short height) {
-        double result = height - 110 * 1.15;
+        double result = (height - 110) * 1.15;
         return result;
     }
 
