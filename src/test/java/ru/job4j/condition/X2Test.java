200b2dfb62cba6e33f281a64a7f8f9e1e68e3821
ruzz3ll <krrsea@gmail.com>
2023-10-08 23:15:42 +0300

    1.2. Входные данные в тесте [#297750 #450815]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -2,7 +2,6 @@ package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class X2Test {
 
