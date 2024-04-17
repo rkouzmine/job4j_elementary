99d2cce71da63374ca3a899db2751df34c194b0f
rkouzmine <krrsea@gmail.com>
2024-04-18 00:22:21 +0300

    Codium. Нейросети спешат на помощь

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -2,8 +2,8 @@ package ru.job4j.array;
 
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
-        int i = 0;
-        int j = array.length - 1;
+        var i = 0;
+        var j = array.length - 1;
         while (i < j) {
             if (array[i] + array[j] == target) {
                 return new int[]{i, j};
